877fc4bc0c87c2f1073982325b4c569a55c93716
ethan <izawaryosuke0101@gmail.com>
2021-04-07 23:05:44 +0900

    dom

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 const google = document.getElementById("google");
+console.log(google);
+google.innerHTML = "goooogle";
+
+const ul = document.getElementById("ul");
+
+const li = document.querySelectorAll("li");
+console.log(li);
+console.log(li[0]);
+li[0].style.backgroundColor = "yellow";
